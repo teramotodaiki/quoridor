30017880d969aab7423a7b66172d6a5f4489a6cf
teramotodaiki <i101323@gmail.com>
2023-10-22 22:32:45 +0900

    駒を出す

--- src/quoridor/init.ts
+++ src/quoridor/init.ts
@@ -12,19 +12,8 @@ export async function init(canvas: HTMLCanvasElement) {
   });
 
   // load the texture we need
-  const texture = await Assets.load("assets/sample.png");
-
-  // This creates a texture from a 'bunny.png' image
-  const bunny = new Sprite(texture);
-  console.log(bunny);
-
-  // Setup the position of the bunny
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
-
-  // Rotate around the center
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
+  const textureWhite = await Assets.load("assets/piece_white.png");
+  const textureBlack = await Assets.load("assets/piece_black.png");
 
   // 9x9マスの盤面を作る
   const board = new Container();
@@ -46,23 +35,15 @@ export async function init(canvas: HTMLCanvasElement) {
   }
   app.stage.addChild(board);
 
-  // Add the bunny to the scene we are building
-  app.stage.addChild(bunny);
+  const pieceWhite = new Sprite(textureWhite);
+  pieceWhite.x = 4 * 64;
+  pieceWhite.y = 0 * 64;
+  board.addChild(pieceWhite);
 
-  // // Listen for frame updates
-  // app.ticker.add(() => {
-  //   // each frame we spin the bunny around a bit
-  //   bunny.rotation += 0.01;
-  // });
-  // Add a variable to count up the seconds our demo has been running
-  let elapsed = 0.0;
-  // Tell our application's ticker to run a new callback every frame, passing
-  // in the amount of time that has passed since the last tick
-  app.ticker.add((delta) => {
-    // Add the time to our total elapsed time
-    elapsed += delta;
-    // Update the sprite's X position based on the cosine of our elapsed time.  We divide
-    // by 50 to slow the animation down a bit...
-    bunny.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-  });
+  const pieceBlack = new Sprite(textureBlack);
+  pieceBlack.x = 4 * 64;
+  pieceBlack.y = 8 * 64;
+  board.addChild(pieceBlack);
+
+  app.ticker.add((delta) => {});
 }
