0acb1cdce1e1fcb5ef57a661a5396f6a947abfaa
teramotodaiki <i101323@gmail.com>
2023-10-23 12:47:54 +0900

    盤の背景

--- src/quoridor/init.ts
+++ src/quoridor/init.ts
@@ -20,21 +20,20 @@ export async function init(canvas: HTMLCanvasElement) {
   const board = new Container();
   board.x = 32;
   board.y = 32;
-  for (let y = 0; y <= 9; y++) {
+  for (let y = 1; y < 9; y++) {
     const line = new Graphics();
     line.lineStyle(1, 0x000000, 1);
     line.moveTo(0, y * 64);
     line.lineTo(576, y * 64);
     board.addChild(line);
   }
-  for (let x = 0; x <= 9; x++) {
+  for (let x = 1; x < 9; x++) {
     const line = new Graphics();
     line.lineStyle(1, 0x000000, 1);
     line.moveTo(x * 64, 0);
     line.lineTo(x * 64, 576);
     board.addChild(line);
   }
-  app.stage.addChild(board);
 
   const pieceWhite = new Sprite(textureWhite);
   pieceWhite.x = 4 * 64;
@@ -52,6 +51,17 @@ export async function init(canvas: HTMLCanvasElement) {
 
   const wall2 = new Wall(5, 1, "horizontal");
   board.addChild(wall2);
+
+  const background = new Sprite(await Assets.load("assets/background.png"));
+  background.pivot.x = 512;
+  background.pivot.y = 512;
+  background.width = 1024 * 1.05;
+  background.height = 1024 * 1.05;
+  background.x = 320 + 4;
+  background.y = 320 + 42;
+  app.stage.addChild(background);
+
+  app.stage.addChild(board);
 
   app.ticker.add((delta) => {});
 }
