40c2105d53b12b59517714d66649284a4e8385d0
teramotodaiki <i101323@gmail.com>
2023-10-24 15:25:09 +0900

    anchorとpivotを両方使うと良い感じ

--- src/quoridor/piece.ts
+++ src/quoridor/piece.ts
@@ -3,9 +3,10 @@ import { Sprite, Texture } from "pixi.js";
 export class Piece extends Sprite {
   constructor(texture: Texture, x: number, y: number) {
     super(texture);
-    this.pivot.x = texture.width / 2;
-    this.pivot.y = texture.height / 2;
-    this.x = x * 64 + 32;
-    this.y = y * 64 + 16;
+    this.anchor.set(0.5);
+    this.pivot.x = -32;
+    this.pivot.y = -16;
+    this.x = x * 64;
+    this.y = y * 64;
   }
 }
