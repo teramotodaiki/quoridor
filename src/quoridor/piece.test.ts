bb47830380296d901fd1a96cd5eef5e8f138fe06
teramotodaiki <i101323@gmail.com>
2023-10-29 22:53:35 +0900

    黒バージョンも作成

--- src/quoridor/piece.test.ts
+++ src/quoridor/piece.test.ts
@@ -40,29 +40,56 @@ describe("getSelectables", () => {
       [5, 0],
       [4, 1],
     ]);
+    expect(stage.get(1)).toStrictEqual([
+      [4, 7],
+      [3, 8],
+      [5, 8],
+    ]);
   });
 
   test("a horizontal wall is by the white", () => {
     const stage = mockStage();
+    // 白の前に壁を置く
     stage.addWall(4, 1, "horizontal");
-
+    // 横にだけ移動できる
     expect(stage.get(0)).toStrictEqual([
       [3, 0],
       [5, 0],
     ]);
-
     // ひとつ右のマスにおいても結果は同じ
     stage.walls[0].X = 5;
     expect(stage.get(0)).toStrictEqual([
       [3, 0],
       [5, 0],
     ]);
-
-    // もう一方のプレイヤーは自由に動ける
+    // 黒は自由に動ける
     expect(stage.get(1)).toStrictEqual([
       [4, 7],
       [3, 8],
       [5, 8],
+    ]);
+  });
+
+  test("a horizontal wall is by the black", () => {
+    const stage = mockStage();
+    // 黒の前も壁を置く
+    stage.addWall(4, 8, "horizontal");
+    // 横にだけ移動できる
+    expect(stage.get(1)).toStrictEqual([
+      [3, 8],
+      [5, 8],
+    ]);
+    // ひとつ右のマスにおいても結果は同じ
+    stage.walls[0].X = 5;
+    expect(stage.get(1)).toStrictEqual([
+      [3, 8],
+      [5, 8],
+    ]);
+    // 白は自由に動ける
+    expect(stage.get(0)).toStrictEqual([
+      [3, 0],
+      [5, 0],
+      [4, 1],
     ]);
   });
 });
