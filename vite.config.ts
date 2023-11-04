3cf5ec72338159a81fc27b5f3e75f1bae5235c3d
teramotodaiki <i101323@gmail.com>
2023-11-04 17:52:16 +0900

    VitestでPixi.jsが動くようにする

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    setupFiles: ["vitest.setup.ts"],
+    environment: "jsdom",
+    threads: false, // https://github.com/vitest-dev/vitest/issues/740
+  },
+});
