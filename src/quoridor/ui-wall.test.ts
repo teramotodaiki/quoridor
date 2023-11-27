a420b67d6591d31d2474df1cd5a8c1e15c35dc8f
teramotodaiki <i101323@gmail.com>
2023-11-27 09:29:09 +0900

    PCから操作（mouseenter）される場合のテスト

--- src/quoridor/ui-wall.test.ts
+++ src/quoridor/ui-wall.test.ts
@@ -1,16 +1,16 @@
-import { Container } from "pixi.js";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { UIWall, createUIWalls } from "./ui-wall";
 
 describe("UIWAll", () => {
   test("new UIWall", () => {
-    const horizontal = new UIWall(1, 2, "horizontal");
+    const onTap = () => {};
+    const horizontal = new UIWall(1, 2, "horizontal", onTap);
     expect(horizontal.X).toBe(1);
     expect(horizontal.Y).toBe(2);
     expect(horizontal.direction).toBe("horizontal");
     expect(UIWall.get(1, 2, "horizontal")).toBe(horizontal);
 
-    const vertical = new UIWall(3, 4, "vertical");
+    const vertical = new UIWall(3, 4, "vertical", onTap);
     expect(vertical.X).toBe(3);
     expect(vertical.Y).toBe(4);
     expect(vertical.direction).toBe("vertical");
@@ -20,9 +20,36 @@ describe("UIWAll", () => {
 
 describe("createUIWalls", () => {
   test("UIWallが作られること", () => {
-    const nextPlayer = () => {};
-    const board = new Container();
-    const uiWallContainer = createUIWalls({ nextPlayer, board });
+    const uiWallContainer = createUIWalls({ onTap() {} });
     expect(uiWallContainer.children.length).toBe(128);
+  });
+
+  test("PCから操作する場合", () => {
+    const onTap = vi.fn();
+    createUIWalls({ onTap });
+    const hori = UIWall.get(1, 1, "horizontal")!;
+    const vert = UIWall.get(1, 1, "vertical")!;
+
+    hori.onmouseenter();
+    expect(hori.alpha).toBe(1);
+
+    hori.onmouseleave();
+    expect(hori.alpha).toBe(0);
+
+    vert.onmouseenter();
+    expect(vert.alpha).toBe(1);
+
+    hori.onmouseenter();
+    expect(vert.alpha).toBe(0);
+    expect(hori.alpha).toBe(1);
+
+    hori.onpointertap();
+    expect(onTap).toHaveBeenCalledOnce();
+    expect(onTap).toHaveBeenCalledWith(hori);
+
+    expect(hori.eventMode).toBe("none");
+    expect(vert.eventMode).toBe("none");
+
+    expect(UIWall.get(2, 1, "horizontal")!.eventMode).toBe("none");
   });
 });
